8348f817a7a78b7d6ef141f30bb1dae8db77f994
RaphTPLR <fiinnnhh@gmail.com>
2022-12-16 09:48:43 +0100

    init

---
+++ display.js
@@ -0,0 +1,211 @@
+class Display { 
+    constructor() {
+        this.game = new Game()
+        this.vert = document.querySelector(".green") // 0
+        this.rouge = document.querySelector('.red') // 1
+        this.bleu = document.querySelector('.blue') // 2
+        this.jaune = document.querySelector('.yellow') // 3
+        this.colorButtons = [this.vert, this.rouge, this.bleu, this.jaune]
+        this.colorOn = ["rgb(138, 235, 140)", "rgb(244, 126, 126)", "rgb(144, 146, 230)", "rgb(237, 235, 117)"]
+        this.start = document.querySelector('.center')
+        this.reset = document.querySelector('.reset')
+        this.dmBtn = document.querySelector('.dmBtn')
+        this.soundBtn = document.querySelector('.soundBtn')
+        this.isUserTurn = false
+        this.currentSound = true
+        this.currentMode = false
+        this.gameStart = false
+        this.round = 1
+        this.addListener()
+        
+    }
+    
+    addListener() {
+        this.start.addEventListener('click', () => {
+            this.gameStart = true
+            this.game.score = 0
+            $(".score").text("SCORE : " + this.game.score)
+            this.game.createCombinaison()
+            this.readCombinaison()
+        })
+        
+        this.reset.addEventListener('click', () => {
+            location.reload()
+
+        })
+        
+        this.soundBtn.addEventListener("click", () => { 
+            if(this.currentSound === true) {
+                this.currentSound = false
+            }else {
+                this.currentSound = true
+            }
+            this.game.audioGestion(this.currentSound, this.currentMode)
+        })
+
+        this.dmBtn.addEventListener("click", () => {
+            console.log(this.currentSound, this.currentMode)
+            if(this.currentMode === true) {
+                this.currentMode = false
+            }else {
+                this.currentMode = true
+            }
+            this.darkMode()
+        })
+
+        this.colorButtons.forEach((button, index) => {
+            button.addEventListener("click", () => {
+                if (this.isUserTurn) {
+                    this.game.addNewUserElement(index)
+                    this.game.compareCombinaison()
+                    this.game.verifScore()
+                    this.restartRound()
+                }           
+            })
+        })
+        
+        this.vert.addEventListener("click", () =>{
+            $('.green').css('background-color','rgb(138, 235, 140)')
+            setTimeout(() => {
+                $('.green').css('background-color','rgba(0, 0, 0, 0)')
+            }, 300)
+        })
+
+        this.rouge.addEventListener("click", () =>{
+            $('.red').css('background-color','rgb(244, 126, 126)')
+            setTimeout(() => {
+                $('.red').css('background-color','rgba(0, 0, 0, 0)')
+            }, 300)
+        })
+        
+        this.bleu.addEventListener("click", () =>{
+            $('.blue').css('background-color','rgb(144, 146, 230)')
+            setTimeout(() => {  
+                $('.blue').css('background-color','rgba(0, 0, 0, 0)')
+            }, 300)
+        })
+
+        this.jaune.addEventListener("click", () =>{
+            $('.yellow').css('background-color','rgb(237, 235, 117)')
+            setTimeout(() => {
+                $('.yellow').css('background-color','rgba(0, 0, 0, 0)')
+            }, 300)
+        })
+    }
+
+    restartRound() {
+        if (this.game.compareCombinaison() === true) {
+            $(".status").text("WIN !").css("border", "3px solid rgb(138, 235, 140)").css("color", "rgb(138, 235, 140)")
+            let round = this.round
+            round++
+            this.round = round
+            setTimeout(() => {
+                this.game.combinaison.push(Math.floor(Math.random() * 4))
+                this.readCombinaison()
+                $("h1").text("LVL : " + round).css("font-size", "20px").css("margin-top", "37px")
+                // $(".")
+            }, 1500);
+            this.isUserTurn = true
+
+        }
+    }
+    // this.rouge.style.backgroundColor = "000000"
+
+
+    readCombinaison() {
+        // $('.container').attr('class','pointer-events: none') // ICI LES BOUTONS NE SE DESACTIVE PAS
+        let index = 0
+        this.isUserTurn = false
+        $(".status").text("READING...").css('border', '3px solid rgb(144, 146, 230)').css("color", "rgb(144, 146, 230)")
+        const id = setInterval(() => {
+            // console.log(index, this.game.combinaison[index])
+            const colorCode = this.game.combinaison[index]
+            this.colorButtons[colorCode].style.backgroundColor = this.colorOn[colorCode]
+            setTimeout(() => {
+                this.turnColorOff(colorCode)
+            }, 300)
+            index++
+            if (index >= this.game.combinaison.length) {
+                clearInterval(id)
+                this.isUserTurn = true
+                if (this.isUserTurn === true) {
+                    $(".status").text("REPEAT !").css('border', '3px solid rgb(237, 235, 117)').css("color", "rgb(237, 235, 117)")
+                    
+                }
+            }
+        }, 1000);
+        this.game.resetCombi()
+    }
+
+    turnColorOff(colorIndex) {
+        console.log("==>", colorIndex, this.colorButtons[colorIndex])
+        this.colorButtons[colorIndex].style.backgroundColor = "rgba(0, 0, 0, 0)"
+    }
+
+    darkMode() {
+        if(this.currentMode === true) {
+            $(".dmBtn").attr("src", "/img/moon-normal-full.png")
+            $("body").css("background-color", "white").css("color", "hsl(213, 20%, 18%)")
+            $("h1")
+            $(".center").css("background-color", "white")
+            $(".reset").css("border", "3px solid hsl(30, 1%, 70%)").css("color", "hsl(213, 20%, 18%)")
+            this.game.audioGestion(this.currentSound, this.currentMode)
+            $(".title").hover(function() {
+                $(this).css("border-bottom", "2px solid hsl(213, 20%, 18%)")
+            }, function() {
+                $(this).css("border-bottom", "2px solid rgba(0, 0, 0, 0)")
+            })
+            $(".center").hover(function() {
+                $(this).css("border", "5px solid hsl(213, 20%, 18%)")
+            }, function() {
+                $(this).css("border", "5px solid hsl(30, 1%, 70%)")
+            })
+            $(".reset").hover(function() {
+                $(this).css("border", "3px solid hsl(213, 20%, 18%)").css("color", "hsl(213, 20%, 18%)" )
+            }, function() {
+                $(this).css("border", "3px solid hsl(30, 1%, 70%)").css("color", "hsl(213, 20%, 18%)" )
+            })
+            if(this.gameStart === false) {
+                $(".status").css("border", "3px solid hsl(30, 1%, 70%)").css("color", "hsl(213, 20%, 18%)")
+                $(".status").hover(function() {
+                    $(this).css("border", "3px solid hsl(213, 20%, 18%)").css("color", "hsl(213, 20%, 18%)" )
+                }, function() {
+                    $(this).css("border", "3px solid hsl(30, 1%, 70%)").css("color", "hsl(213, 20%, 18%)" )
+                })
+            }
+
+        } else if(this.currentMode === false) {
+            $(".dmBtn").attr("src", "/img/moon-dm-normal.png")
+            $("body").css("background-color", "hsl(213, 20%, 18%)").css("color", "wheat")
+            $(".center").css("background-color", "hsl(213, 20%, 18%)")
+            $(".reset").css("border", "3px solid hsl(30, 1%, 70%)").css("color", "wheat")
+            this.game.audioGestion(this.currentSound, this.currentMode)
+            $(".title").hover(function() {
+                $(this).css("border-bottom", "2px solid wheat")
+            }, function() {
+                $(this).css("border-bottom", "2px solid rgba(0, 0, 0, 0)")
+            })
+            $(".center").hover(function() {
+                $(this).css("border", "5px solid wheat")
+            }, function() {
+                $(this).css("border", "5px solid hsl(30, 1%, 70%)")
+            })
+            $(".reset").hover(function() {
+                $(this).css("border", "3px solid wheat").css("color", "hsl(30, 1%, 70%)" )
+            }, function() {
+                $(this).css("border", "3px solid hsl(30, 1%, 70%").css("color", "wheat" )
+            })
+            
+            if(this.gameStart === false) {
+                $(".status").css("border", "3px solid hsl(30, 1%, 70%)").css("color", "wheat")
+                $(".status").hover(function() {
+                    $(this).css("border", "3px solid wheat").css("color", "hsl(30, 1%, 70%)" )
+                }, function() {
+                    $(this).css("border", "3px solid hsl(30, 1%, 70%)").css("color", "wheat" )
+                })
+            }
+        }
+    }
+}
+
+const display = new Display()
---
+++ index.js
@@ -0,0 +1,99 @@
+class Game { 
+    constructor() {
+        this.combinaison = []
+        this.userCombinaison = []
+        this.score = 0
+        this.longueur = 1
+        this.round = 1
+        this.audio = document.querySelector("audio")
+        this.audio.volume = 0.2
+
+
+        // this.vert = document.getElementById("green").style.backgroundColor;
+
+        // console.log(this.color)
+    }
+
+    createCombinaison() {
+        let combinaison = []
+        for (let i = 0; i < this.longueur; i++) {
+            combinaison.push(Math.floor(Math.random() * 4))
+        }
+        $("h1").text("LVL : " + this.round).css("font-size", "20px").css("margin-top", "37px")
+        $(".compteur").css("color", "wheat")
+        console.log(combinaison)
+        this.combinaison = combinaison
+    }
+
+    compareCombinaison() {
+        if (this.combinaison.length == this.userCombinaison.length) {
+            for (let i = 0; i < this.combinaison.length; i++) {
+                if (this.combinaison[i] != this.userCombinaison[i]) {
+                    $(".status").text("LOOSE...").css("border", "3px solid rgb(244, 126, 126)").css("color", "rgb(244, 126, 126)")
+                    $("h1").text("RESTART").css("font-size", "20px").css("margin-top", "37px")
+                    $(".compteur").css("color", "transparent")
+                    return false
+                }
+            }
+            $(".status").text("WIN !").css("border", "3px solid rgb(138, 235, 140)").css("color", "rgb(138, 235, 140)")
+            return true
+        }
+    }
+    addNewUserElement(el) {
+        if (this.userCombinaison.length < this.combinaison.length) {
+            this.userCombinaison.push(el)
+        }
+        console.log("user combinaison : ", el, this.userCombinaison)
+    }
+
+    resetCombi() {
+        this.userCombinaison = []
+    }
+
+    verifScore() {
+        let score = this.score
+        if (this.combinaison.length == this.userCombinaison.length) {
+            for (let i = 0; i < this.combinaison.length; i++) {
+                if (this.combinaison[i] != this.userCombinaison[i]) {
+                    break
+                } else {
+                    score++
+                }
+            }
+        }
+        this.score = score
+        $(".score").text("SCORE : " + this.score)
+    }
+
+    audioGestion(currentSound, currentMode) {
+        if(currentMode === false) {
+            if(currentSound === true) {
+                this.audio.volume = 0.2
+                $(".soundBtn").attr("src", "/img/sound-dm-full .png")
+            }else if(currentSound === false){
+                this.audio.volume = 0
+                $(".soundBtn").attr("src", "/img/sound-dm-mute.png")
+            }
+        } else if(currentMode === true){
+            if(currentSound === true) {
+                this.audio.volume = 0.3
+                $(".soundBtn").attr("src", "/img/sound-normal-full.png")
+            }else if(currentSound === false){
+                this.audio.volume = 0
+                $(".soundBtn").attr("src", "/img/sound-normal-mute.png")
+            }
+        }
+    }
+
+
+    // lightVert() {
+    // this.vert.style.backgroundColor = rgb(138, 235, 140);  
+    // }
+
+    // light() {
+    //     for( let i = 0; i < this.color.length; i++){
+    //         this.color[i].style.backgroundColor = "white"
+    //     }
+    // }
+
+}
